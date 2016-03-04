ed7b8d2ac86843969bf0d129f216ba0a912b9fbf
Nareg Khoshafian <nareg@intrepid.io>
2016-03-04 09:32:30 -0500

    Initial Commit

---
+++ Travel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by Nareg Khoshafian on 3/4/16.
+//  Copyright © 2016 Intrepid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
