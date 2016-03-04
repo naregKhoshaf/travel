8d2ea538ba348760d6b8152d12c78463fa476931
Nareg Khoshafian <nareg@intrepid.io>
2016-03-04 15:51:23 -0500

    Setup.

---
+++ Travel/MyTripsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyTripsCollectionViewCell.swift
+//  Travel
+//
+//  Created by Nareg Khoshafian on 3/4/16.
+//  Copyright © 2016 Intrepid. All rights reserved.
+//
+
+import UIKit
+
+class MyTripsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
