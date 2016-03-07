f8d391a3d7ae775f22bd23a8390ba387848a0958
Nareg Khoshafian <nareg@intrepid.io>
2016-03-07 12:58:38 -0500

    Notes on methods and clean up.

--- Travel/MyTripsViewController.swift
+++ Travel/MyTripsViewController.swift
@@ -9,13 +9,6 @@
 import Foundation
 import UIKit
 
-
-protocol CardCarouselDelegate: class {
-    func viewForCarousel(carousel: MyTripsViewController, atIndex index: NSInteger) -> UIView
-    func numberOfItemsInCarousel(carousel: MyTripsViewController) -> NSInteger
-    func carousel(carousel: MyTripsViewController, didSelectOrbAtIndex index: NSInteger)
-}
-
 class MyTripsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var tripsCollectionView: UICollectionView!
@@ -30,7 +23,6 @@ class MyTripsViewController: UIViewController, UICollectionViewDataSource, UICol
         tripsCollectionView.registerNib(cellNib, forCellWithReuseIdentifier: reuseIdentifier)
     }
     
-    
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = tripsCollectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MyTripsCollectionViewCell
         let photo = imageArray[indexPath.row]
@@ -42,70 +34,68 @@ class MyTripsViewController: UIViewController, UICollectionViewDataSource, UICol
         return imageArray.count
     }
     
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let screenWidth = CGRectGetWidth(collectionView.bounds)
-        let cellWidth = screenWidth
-        return CGSize(width: cellWidth - 30, height: cellWidth - 30)
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 1
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 1
-    }
-    
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         print(1)
     }
 }
 
 
-class CarouselCollectionViewLayout: UICollectionViewFlowLayout {
-    let sizeItem = CGSize(width: 133, height: 173)
+class CarouselCollectionViewLayout: UICollectionViewLayout {
+    // This is a subclass of layout and as a result we need to handle all the parts of the Layout
+    let itemSize = CGSize(width: 300, height: 300)
+    
+    var angleAtExtreme: CGFloat {
+        return collectionView!.numberOfItemsInSection(0) > 0 ?
+            -CGFloat(collectionView!.numberOfItemsInSection(0) - 1) * anglePerItem : 0
+    }
+    var angle: CGFloat {
+        return angleAtExtreme * collectionView!.contentOffset.x / (collectionViewContentSize().width -
+            CGRectGetWidth(collectionView!.bounds))
+    }
     
     var radius: CGFloat = 500 {
         didSet {
-            invalidateLayout() //When the radius changes, recalculate everything
+            invalidateLayout()
         }
-    }
+    } //When the radius changes, recalculate everything
     
     var anglePerItem: CGFloat {
-        return atan(itemSize.width / radius) // This ensures cells aren't spread too far apart
+        return atan((itemSize.width + 100) / radius) // This ensures cells aren't spread too far apart
     }
     
+    // Holds layout attribute instances
     var attributesList = [CarouselCollectionViewLayoutAttributes]()
     
+    // Declare how big the Content of your collection should be.
     override func collectionViewContentSize() -> CGSize {
         return CGSize(width: CGFloat(collectionView!.numberOfItemsInSection(0)) * itemSize.width,
             height: CGRectGetHeight(collectionView!.bounds))
     }
     
+    // Tells the collection view that we will be using the CarouselCollectionViewLayoutAttributes and not the default Layout Attributes.
     override class func layoutAttributesClass() -> AnyClass {
         return CarouselCollectionViewLayoutAttributes.self
     }
     
+    // prepareLayout is the first method called when the collection view apperas on screen.  This is where you create and store layout attributes. Very Important. 
+    // You iterate over each item in the collection view and execute the closure.
     override func prepareLayout() {
         super.prepareLayout()
         
         let centerX = collectionView!.contentOffset.x + (CGRectGetWidth(collectionView!.bounds) / 2.0)
         let anchorPointY = ((itemSize.height / 2.0) + radius) / itemSize.height
         
+        // map here creates a new array with the reuslts of the closure for each element in the range.
         attributesList = (0..<collectionView!.numberOfItemsInSection(0)).map { (i)
             -> CarouselCollectionViewLayoutAttributes in
-            // 1
+            // 1 Create an instance of CoureselCollectionViewLayoutAttributes for each index path, and then set its size
             let attributes = CarouselCollectionViewLayoutAttributes(forCellWithIndexPath: NSIndexPath(forItem: i,
                 inSection: 0))
             attributes.size = self.itemSize
-            // 2
+            // 2 Position each item at the center of the screen
             attributes.center = CGPoint(x: centerX, y: CGRectGetMidY(self.collectionView!.bounds))
-            // 3
-            attributes.angle = self.anglePerItem*CGFloat(i)
+            // 3 Rotate each item by the amount anglePerItem * i
+            attributes.angle = self.angle + (self.anglePerItem * CGFloat(i))
             
             attributes.anchorPoint = CGPoint(x: 0.5, y: anchorPointY)
 
@@ -113,27 +103,36 @@ class CarouselCollectionViewLayout: UICollectionViewFlowLayout {
         }
     }
     
+    // Returns an array of layout attributes for all of the cells and views in the specified rectangle.
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         return attributesList
     }
     
+    // Returns the attributes for the item at the given idex path.
     override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath)
         -> UICollectionViewLayoutAttributes? {
             return attributesList[indexPath.row]
     }
+    
+    override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
+        return true
+    }
 }
 
 class CarouselCollectionViewLayoutAttributes: UICollectionViewLayoutAttributes {
+    // I subclassed collectionViewLayout but I also have to subclass the CollectionViewLayoutsAttributes
 
+    // 1. Anchor Point is where the rotation happens around which isn't the center.
     var anchorPoint = CGPoint(x: 0.5, y: 0.5)
-    var angle: CGFloat = 0 {
     
+    // 2. Transform willl be equal to the the angle.
+    var angle: CGFloat = 0 {
         didSet {
-            zIndex = Int(angle * 1000000)
             transform = CGAffineTransformMakeRotation(angle)
         }
     }
     
+    // 3. UICollecionViewLayoutAttributes  needs to conform to NSCopying protocol because the attribute's objects can be copied internally when the collection view is performing a layout.  Override this method to gurantee that both the anchorPoint and angle properties are set when the object is copied.
     override func copyWithZone(zone: NSZone) -> AnyObject {
         let copiedAttributes: CarouselCollectionViewLayoutAttributes = super.copyWithZone(zone) as! CarouselCollectionViewLayoutAttributes
         copiedAttributes.anchorPoint = self.anchorPoint
